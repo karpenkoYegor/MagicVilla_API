beea830557d6d18fcd0138d569af44b43acb60c5
Kodywin <kodywinq1@gmail.com>
2023-03-10 22:32:54 +0300

    add: post method

--- MagicVilla_VillAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillAPI/Controllers/VillaAPIController.cs
@@ -1,12 +1,51 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Model;
+using MagicVilla_VillaAPI.Model.DTO;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaAPI.Controllers
 {
-    public class VillaAPIController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
     {
-        public IActionResult Index()
+        [HttpGet]
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
         {
-            return View();
+            return Ok(VillaStore.villaList);
+        }
+
+        [HttpGet("{id:int}", Name = "GetVilla")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<VillaDTO> GetVillas(int id)
+        {
+            var villa = VillaStore.villaList.SingleOrDefault(u => u.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(villa);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<VillaDTO> CreateVilla([FromBody]VillaDTO villa)
+        {
+            if (villa == null)
+            {
+                return BadRequest(villa);
+            }
+            if (villa.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            villa.Id = VillaStore.villaList.OrderByDescending(u => u.Id).First().Id + 1;
+            VillaStore.villaList.Add(villa);
+            return CreatedAtRoute("GetVilla", new {id = villa.Id}, villa);
         }
     }
 }
